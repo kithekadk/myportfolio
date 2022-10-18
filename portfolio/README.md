8c1f1fc84c3ae99125d031297d244b8db6768737
unknown <dankinyi99@gmail.com>
2022-10-18 18:44:45 +0300

    added images

---
+++ portfolio/README.md
@@ -0,0 +1,6 @@
+# Personal Website
+
+This is my personal, portfolio website built in HTML5, CSS3 and JavaScript.
+
+## Screenshots
+
---
+++ portfolio/web/js/app.js
@@ -0,0 +1,91 @@
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
+// show sidebar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+// set year
+date.innerHTML = new Date().getFullYear();
+
+
+
+//Firebase Database for storing Contact Info.
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD-_LzhS2DDE3JSoyjfV85G_QaL2jXyCBU",
+  authDomain: "personal-blog-ca694.firebaseapp.com",
+  databaseURL: "https://personal-blog-ca694.firebaseio.com",
+  projectId: "personal-blog-ca694",
+  storageBucket: "personal-blog-ca694.appspot.com",
+  messagingSenderId: "912964403902",
+  appId: "1:912964403902:web:3ced5a7c80c5f71ac554ef",
+  measurementId: "G-ZQCR7D4DE7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+} 
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company: company,
+    email: email,
+    phone: phone,
+    message: message
+  });
+}
+ 
